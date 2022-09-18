9d3d23b9bb88101d2d4277e68794844f67410a4b
siva <siva41992@gmail.com>
2022-09-18 14:31:14 +0000

    jfrog_setup_file

---
+++ jfrog_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo su;yum install java-1.8* -y 
+cd /opt 
+wget https://jfrog.bintray.com/artifactory/jfrog-artifactory-oss-6.9.6.zip
+unzip jfrog-artifactory-oss-6.9.6.zip
+/opt/jfrog-artifactory-oss-6.9.6/bin/artifactory.sh start
