ca8cf764e6cc24d1e715aab1d4d7847742d3442f
root <root@ip-172-31-94-95.ec2.internal>
2022-09-18 15:19:22 +0000

    added

--- jfrog_setup.sh
+++ jfrog_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo su;yum install java-1.8* -y 
 cd /opt 
+uname -a
 wget https://jfrog.bintray.com/artifactory/jfrog-artifactory-oss-6.9.6.zip
 unzip jfrog-artifactory-oss-6.9.6.zip
 /opt/jfrog-artifactory-oss-6.9.6/bin/artifactory.sh start
