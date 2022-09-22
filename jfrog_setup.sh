3d8213a7e5df378a7045e62c9608fdf6cef092f4
docker41992 <113501210+docker41992@users.noreply.github.com>
2022-09-22 12:41:41 +0530

    Update jfrog_setup.sh

--- jfrog_setup.sh
+++ jfrog_setup.sh
@@ -6,3 +6,4 @@ wget https://jfrog.bintray.com/artifactory/jfrog-artifactory-oss-6.9.6.zip
 unzip jfrog-artifactory-oss-6.9.6.zip
 /opt/jfrog-artifactory-oss-6.9.6/bin/artifactory.sh start
 uname -a
+uname 
